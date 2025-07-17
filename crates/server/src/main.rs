5a295db26f323da3055b12daff513d8713f29339
Dudley Irish <dudley.irish@gmail.com>
2025-07-17 15:36:43 -0600

    Completed task 1. Closes #1

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -5,6 +5,13 @@ fn index(_req: Request) -> Response {
     Ok(Content::Html(content))
 }
 
+fn chat(_req: Request) -> Response {
+    Ok(Content::Json("{\"messages\": [\"Hello world!\", \"And how does that make you feel?\"]}".to_string()))
+}
+
 fn main() {
-    miniserve::Server::new().route("/", index).run()
+    let mut server = miniserve::Server::new();
+    server = server.route("/", index);
+    server = server.route("/chat", chat);
+    server.run();
 }
